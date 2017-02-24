cd01131ce20246563048a2b7acab28b5f36f2fd6
AbdalaGabriel <g.abdala.04@gmail.com>
2017-02-24 18:16:09 -0300

    V 0.0.0

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8" />
+		<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+		<link rel="icon" type="image/png" href="assets/img/favicon.png">
+		<link rel="stylesheet" type="text/css" href="style/header.css">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<title>LSD EYE - Plataforma de videos</title>
+		<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+	</head>
+
+	<body>
+
+		<!-- HEADER -->
+		<?php include 'templates/header.php'; ?>
+
+
+	</body>
+
+</html>
---
+++ templates/header.php
@@ -0,0 +1,26 @@
+<header id="lsd-main-header">
+	
+	<div class="left-column">
+		<nav class="channels-container">
+			<a href="#">Canal la red</a>
+			<a href="#">Otros canales</a>
+		</nav>
+	</div>
+
+	<div class="right-column">
+
+		<div class="user">
+			<img class="user-image-profile"></img>
+			<span class="user-mail">usuario@gmail.com</span>
+		</div>
+
+		<div class="configurations">
+			<img class="config-icon"></img>
+		</div>
+
+		<div class="main-menu">
+			<img class="main-menu-icon"></img>
+		</div>
+
+	</div>
+</header>
