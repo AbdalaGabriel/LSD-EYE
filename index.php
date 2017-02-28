2e9021932db9db798221d8de1a5158ca1e185ca0
AbdalaGabriel <g.abdala.04@gmail.com>
2017-02-27 23:34:07 -0300

    v 0.0.2
    Estilos generales menu y header + pantalla real time

--- index.php
+++ index.php
@@ -1,33 +1,327 @@
 <!doctype html>
 <html lang="es">
-	<head>
-		<meta charset="utf-8" />
-		<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
-		<link rel="icon" type="image/png" href="assets/img/favicon.png">
-		<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-		<link rel="stylesheet" type="text/css" href="style/global.css">
-		<link rel="stylesheet" type="text/css" href="style/header.css">
-		<link rel="stylesheet" type="text/css" href="style/lateral-bar.css">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-		<title>LSD EYE - Plataforma de videos</title>
-		<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-	</head>
+<head>
+	<meta charset="utf-8" />
 
-	<body>
+	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+	<link rel="icon" type="image/png" href="assets/img/favicon.png">
 
-		<!-- HEADER -->
-		<?php include 'templates/header.php'; ?>
+	<link rel="stylesheet" href="style/bootstrap/bootstrap.css">
 
-		<!--LATERAL BAR -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+
+
+	<link rel="stylesheet" type="text/css" href="style/global.css">
+	<link rel="stylesheet" type="text/css" href="style/header.css">
+	<link rel="stylesheet" type="text/css" href="style/lateral-bar.css">
+	<link rel="stylesheet" type="text/css" href="style/main.css">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>LSD EYE - Plataforma de videos</title>
+
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+
+</head>
+
+<body>
+
+	<!-- HEADER -->
+	<?php include 'templates/header.php'; ?>
+
+	<!--LATERAL BAR -->
+
+	<aside id="main-lateral-bar">
+		<?php include 'templates/lateral-bar.php'; ?>
+	</aside>
+
+	<main id="main-content">
+		<header>
+			<nav>
+				<ul>
+					<li><a href="#">Vod</a></li>
+					<li><a class="active" href="#">Live</a></li>
+				</ul>
+			</nav>
+		</header>
+
+		<div id="main-container">
+			<div class="row">
+				<div class="col-sm-12 col-md-12 col-lg-4 start-collumn">
+					<div class="panel users-now">
+						<a href="#" class="info" data-placement="left" data-toggle="tooltip" title="Lorem Ipsum sit dolor amet commet"></a>
+						<span class="hour">20:12:57 hs.</span>
+
+						<h1 class="users-quantity">
+							2948
+						</h1>
+
+						<div class="users-label">
+							<span class="icon"></span>
+							<h2>Usuarios ahora</h2>
+						</div>
+					</div>
+
+				</div>
+				<div class="col-sm-12 col-md-12 col-lg-4">
+					<div class="panel platforms-stats">
+						<a href="#" class="info" data-placement="left" data-toggle="tooltip" title="Lorem Ipsum sit dolor amet commet"></a>
+						<div class="platform-container desktop">
+
+							<div class="platform-name">
+								<span class="icon"></span>
+								<h2>Desktop</h2>
+							</div>
+							<h3 class="percentage"><span class="number">70.14</span>%</h3>
+
+							<div class="percentage-bar-container">
+								<div class="hundred-percent"></div>
+
+								<div class="real-time-percentage-bar"></div>
+							</div>
+						</div>
+
+						<div class="platform-container mobile-app">
+							<div class="platform-name">
+								<span class="icon"></span>	
+								<h2>Mobile App</h2>
+							</div>
+
+							<h3 class="percentage"><span class="number">70.14</span>%</h3>
+
+							<div class="percentage-bar-container">
+								<div class="hundred-percent"></div>
+
+								<div class="real-time-percentage-bar"></div>
+							</div>
+						</div>
+
+						<div class="platform-container mobile-web">
+							<div class="platform-name">
+								<span class="icon"></span>
+								<h2>Mobile Web</h2>
+							</div>
+							<h3 class="percentage"><span class="number">70.14</span>%</h3>
+
+							<div class="percentage-bar-container">
+								<div class="hundred-percent"></div>
+
+								<div class="real-time-percentage-bar"></div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+				<div class="col-sm-12 col-md-12 col-lg-4 end-collumn">
+					<div class="panel numeric plays">
+					<a href="#" class="info" data-placement="left" data-toggle="tooltip" title="Lorem Ipsum sit dolor amet commet"></a>
+						<div class="number-container users">
+
+							<h3><span class="icon"></span>12224 </h3>
+						</div>
+						<h2 class="label">Plays acumulados en el día</h2>
+					</div>
+
+					<div class="panel numeric users">
+
+						<div class="number-container">
+
+							<h3><span class="icon"></span>3010</h3>
+						</div>
+						<h2 class="label">Usuarios acumulados en el día</h2>
+					</div>
+
+					<div class="panel numeric hours">
+						<div class="number-container">
+
+							<h3><span class="icon"></span>122 <span class="min-container">min</span></h3>
+						</div>
+						<h2 class="label">Tiempo promedio</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12 col-md-12 col-lg-8 start-collumn">
+					<div class="panel">
+						<h2 class="panel-title real-time-graphic">
+							Tiempo real
+						</h2>
+
+						
+					</div>
+
+					<div class="panel">
+						<h2 class="panel-title countries-graphic">
+							Países
+						</h2>
+					</div>
+				</div>
+				<div class="col-sm-12 col-md-12 col-lg-4 end-collumn">
+					<div class="panel visualization-times">
+						<h2 class="panel-title">
+							Tiempos de visualización
+						</h2>
+
+						<div class="percentage-module">
+							<h2><span class="bold">0-5</span> min (<span class="plays-container">6211</span> PLAYS )</h2>
+							<h3 class="percentage"><span class="number">15.02</span>%</h3>
+
+							<div class="percentage-bar-container">
+								<div class="hundred-percent"></div>
+
+								<div class="real-time-percentage-bar"></div>
+							</div>
+
+						</div>
+
+						<div class="percentage-module">
+							<h2><span class="bold">5-10</span> min (<span class="plays-container">6211</span> PLAYS )</h2>
+							<h3 class="percentage"><span class="number">15.02</span>%</h3>
+
+							<div class="percentage-bar-container">
+								<div class="hundred-percent"></div>
+
+								<div class="real-time-percentage-bar"></div>
+							</div>
+
+						</div>
+
+						<div class="percentage-module">
+							<h2><span class="bold">10-15</span> min (<span class="plays-container">6211</span> PLAYS )</h2>
+							<h3 class="percentage"><span class="number">15.02</span>%</h3>
+
+							<div class="percentage-bar-container">
+								<div class="hundred-percent"></div>
+
+								<div class="real-time-percentage-bar"></div>
+							</div>
+
+						</div>
+
+						<div class="percentage-module">
+							<h2><span class="bold">15-20</span> min (<span class="plays-container">6211</span> PLAYS )</h2>
+							<h3 class="percentage"><span class="number">15.02</span>%</h3>
+
+							<div class="percentage-bar-container">
+								<div class="hundred-percent"></div>
+
+								<div class="real-time-percentage-bar"></div>
+							</div>
+
+						</div>
+
+						<div class="percentage-module">
+							<h2><span class="bold">20-25</span> min (<span class="plays-container">6211</span> PLAYS )</h2>
+							<h3 class="percentage"><span class="number">15.02</span>%</h3>
+
+							<div class="percentage-bar-container">
+								<div class="hundred-percent"></div>
+
+								<div class="real-time-percentage-bar"></div>
+							</div>
+
+						</div>
+
+						<div class="percentage-module">
+							<h2><span class="bold">25-30</span> min (<span class="plays-container">6211</span> PLAYS )</h2>
+							<h3 class="percentage"><span class="number">15.02</span>%</h3>
+
+							<div class="percentage-bar-container">
+								<div class="hundred-percent"></div>
+
+								<div class="real-time-percentage-bar"></div>
+							</div>
+
+						</div>
+
+						<div class="percentage-module">
+							<h2><span class="bold">30-35</span> min (<span class="plays-container">6211</span> PLAYS )</h2>
+							<h3 class="percentage"><span class="number">15.02</span>%</h3>
+
+							<div class="percentage-bar-container">
+								<div class="hundred-percent"></div>
+
+								<div class="real-time-percentage-bar"></div>
+							</div>
+
+						</div>
+
+						<div class="percentage-module">
+							<h2><span class="bold">45-60</span> min (<span class="plays-container">6211</span> PLAYS )</h2>
+							<h3 class="percentage"><span class="number">15.02</span>%</h3>
+
+							<div class="percentage-bar-container">
+								<div class="hundred-percent"></div>
+
+								<div class="real-time-percentage-bar"></div>
+							</div>
+
+						</div>
+
+						<div class="percentage-module">
+							<h2><span class="bold">+ 60</span> min (<span class="plays-container">6211</span> PLAYS )</h2>
+							<h3 class="percentage"><span class="number">15.02</span>%</h3>
+
+							<div class="percentage-bar-container">
+								<div class="hundred-percent"></div>
+
+								<div class="real-time-percentage-bar"></div>
+							</div>
+
+						</div>
+					</div>
+
+					<div class="panel program">
+						<h3>Ahora</h3>
+						<h2 class="panel-title">
+							Nombre del programa
+						</h2>
+
+						<div class="player">
+							<div class="radio-name-container">
+								<a href="#" data-toggle="tooltip" data-placement="bottom"  title="play" class="play"></a>
+								<h4 class="radio-name">Radio La Red</h4>
+							</div>
+
+							<div class="volume-container">
+								<span class="icon"></span>
+								<div class="volume">
+									<div class="volume-bar"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="panel encoder">
+						<a href="#" data-toggle="tooltip" title="Ver encoder">
+						<div class="see-encoder-container">
+							<div class="icon-container">
+								<span class="icon"></span>
+							</div>
+							<div class="texts">
+								<h2>Ver encoder</h2>
+								<h3>Gráficos completos</h3>
+							</div>
+							</div>
+						</a>
+
+					</div>
+				</div>
+			</div>
+
+		</div>
 		
-		<aside id="main-lateral-bar">
-			<?php include 'templates/lateral-bar.php'; ?>
-		</aside>
-		
-		<main>
-			
-		</main>
+	</main>
 
-	</body>
-
+</body>
+<script src="js/tether.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script>
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+</script>
 </html>
